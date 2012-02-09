fe7ca402a976646cc9ee09344945ef9199977e6c
Steven Klise <sklise@gmail.com>
2012-02-09 01:12:54 -0500

    Logging FaceOSC messages to browser console...once

--- app/coffee/server.coffee
+++ app/coffee/server.coffee
@@ -7,16 +7,23 @@ app.listen(3000)
 
 osc = require('./lib/osc');
 
+m = ''
+newmessage = false
 oscServer = new osc.Server(1337, '0.0.0.0')
 oscServer.on "message", (msg, rinfo) ->
-	console.log("TUIO message:")
-	console.log(msg)
+  console.log("TUIO message:")
+  console.log(msg)
+  m = msg
+  newmessage = true
+  io.sockets.on 'connection', (socket) ->
+    socket.emit('news', {hello: 'world'})
+    if newmessage
+      socket.emit 'news', m
+      newmessage = !newmessage
+    socket.on 'my other event', (data) ->
+      console.log data
+
 
 
 app.get '/', (req, res) ->
   res.sendfile(__dirname + '/index.html')
-  
-io.sockets.on 'connection', (socket) ->
-  socket.emit('news', {hello: 'world'})
-  socket.on 'my other event', (data) ->
-    console.log data
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 (function() {
-  var app, fs, io, osc, oscServer, sys;
+  var app, fs, io, m, newmessage, osc, oscServer, sys;
 
   sys = require('sys');
 
@@ -13,24 +13,33 @@
 
   osc = require('./lib/osc');
 
+  m = '';
+
+  newmessage = false;
+
   oscServer = new osc.Server(1337, '0.0.0.0');
 
   oscServer.on("message", function(msg, rinfo) {
     console.log("TUIO message:");
-    return console.log(msg);
+    console.log(msg);
+    m = msg;
+    newmessage = true;
+    return io.sockets.on('connection', function(socket) {
+      socket.emit('news', {
+        hello: 'world'
+      });
+      if (newmessage) {
+        socket.emit('news', m);
+        newmessage = !newmessage;
+      }
+      return socket.on('my other event', function(data) {
+        return console.log(data);
+      });
+    });
   });
 
   app.get('/', function(req, res) {
     return res.sendfile(__dirname + '/index.html');
-  });
-
-  io.sockets.on('connection', function(socket) {
-    socket.emit('news', {
-      hello: 'world'
-    });
-    return socket.on('my other event', function(data) {
-      return console.log(data);
-    });
   });
 
 }).call(this);
